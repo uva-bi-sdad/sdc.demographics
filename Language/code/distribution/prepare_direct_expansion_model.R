4289b2b571030f7baa6bb0c91f47d839af048df0
uva-bi-sdad <>
2023-08-16 19:19:47 +0000

    Update from https://github.com/uva-bi-sdad/sdc.demographics_dev/commit/3167236d5361629eafb077fdb98c9241aa58f82f

---
+++ Language/code/distribution/prepare_direct_expansion_model.R
@@ -0,0 +1,157 @@
+# this file estimate demographics to new geographies by directly expand the census geographies (without refining): use the package redistribute
+
+
+# libraries -------------------------------------------------------------
+library(dplyr)
+library(sf)
+# library(httr)
+library(sp)
+library(data.table)
+library(stringr)
+#library("rgdal", lib.loc="/usr/local/lib/R/site-library")
+library(tidyr)
+library(readr)
+# library(tidyverse)
+library(tidycensus)
+library(tigris)
+library(rjson)
+library(redistribute)
+
+
+# load the data -------------------------------------------------------------------
+# get the age demographics acs data for virginia
+uploadpath = "Language/data/working/"
+files = list.files(uploadpath)
+filename = files[str_detect(files,"va_cttrbg_acs")]
+acs <- readRDS(paste0(uploadpath,filename))
+
+# prepare the data for modeling -------------------------------------------
+# select the census block group for aggregation, select only population count as measure
+acs_bg <- acs %>%
+  filter(region_type=='tract') %>%
+  select(geoid,year,measure,value) %>%
+  #mutate(measure=str_replace(measure,'language_','')) %>%
+  pivot_wider(names_from = measure, values_from = value) %>%
+  mutate(total_hh =100*hh_limited_english/perc_hh_limited_english, 
+         census_year=if_else(year<2020,2010,2020), geoid=as.numeric(geoid)) 
+
+
+# get census 2010 and 2020
+# bg_geo_2010 <- sf::read_sf('https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/VA/Census%20Geographies/Block%20Group/2010/data/distribution/va_geo_census_cb_2010_census_block_groups.geojson')
+# bg_geo_2020 <- sf::read_sf('https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/VA/Census%20Geographies/Block%20Group/2020/data/distribution/va_geo_census_cb_2020_census_block_groups.geojson')
+# bg_geo <- rbind(bg_geo_2010,bg_geo_2020) %>%
+#   select(geoid,year,geometry) %>%
+#   mutate(census_year=as.numeric(year))
+
+bg_geo_2010 <- sf::read_sf('https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/VA/Census%20Geographies/Tract/2010/data/distribution/va_geo_census_cb_2010_census_tracts.geojson')
+bg_geo_2020 <- sf::read_sf('https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/VA/Census%20Geographies/Tract/2020/data/distribution/va_geo_census_cb_2020_census_tracts.geojson')
+bg_geo <- rbind(bg_geo_2010,bg_geo_2020) %>% select(geoid,year,geometry) %>% 
+  mutate(census_year=as.numeric(year),
+         geoid=as.numeric(geoid)) %>%
+  select(-year)
+
+temp_acs <- merge(acs_bg,bg_geo, by=c('geoid','census_year')) 
+temp_acs_sf <- st_as_sf(temp_acs)
+
+# load new geographies geometries
+hsr_geo <- sf::st_read("https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/VA/Local%20Geographies/Fairfax%20County/Human%20Services%20Regions/2022/data/distribution/va059_geo_ffxct_gis_2022_human_services_regions.geojson")
+pd_geo <- sf::st_read("https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/VA/Local%20Geographies/Fairfax%20County/Planning%20Districts/2022/data/distribution/va059_geo_ffxct_gis_2022_planning_districts.geojson")
+sd_geo <- sf::st_read("https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/VA/Local%20Geographies/Fairfax%20County/Supervisor%20Districts/2022/data/distribution/va059_geo_ffxct_gis_2022_supervisor_districts.geojson")
+zc_geo <- sf::st_read("https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/VA/Local%20Geographies/Fairfax%20County/Zip%20Codes/2022/data/distribution/va059_geo_ffxct_gis_2022_zip_codes.geojson")
+civic_geo <- sf::st_read("https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/VA/Local%20Geographies/Arlington%20County/Civic%20Associations/2021/data/distribution/va013_geo_arl_2021_civic_associations.geojson")
+hd_geo <- sf::st_read('https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/VA/State%20Geographies/Health%20Districts/2020/data/distribution/va_geo_vhd_2020_health_districts.geojson')
+pd_geo <- sf::st_make_valid(pd_geo)
+
+# get label and geometry separetly
+temp_hsr <- hsr_geo %>% select(geoid_hsr=geoid,geometry)
+temp_pd <- pd_geo %>% select(geoid_pd=geoid,geometry)
+temp_sd <- sd_geo %>% select(geoid_sd=geoid,geometry)
+temp_zc <- zc_geo %>% select(geoid_zc=geoid,geometry)
+temp_civic <- civic_geo %>% select(geoid_ca=geoid,geometry)
+temp_hd <- hd_geo %>% select(geoid_hd=geoid,geometry)
+
+
+# redistribute the population ---------------------------------------------------
+yearlist <- unique(temp_acs_sf$year)
+model <- NULL
+for (vyear in yearlist){
+  subtemp_acs_sf <- temp_acs_sf %>% filter(year==vyear) %>% select(-c('census_year','year'))
+  predict_hsr <- redistribute(subtemp_acs_sf, temp_hsr, source_id = "geoid", target_id = 'geoid_hsr')
+  predict_pd <- redistribute(subtemp_acs_sf, temp_pd, source_id = "geoid", target_id = 'geoid_pd')
+  predict_sd <- redistribute(subtemp_acs_sf, temp_sd, source_id = "geoid", target_id = 'geoid_sd')
+  predict_zc <- redistribute(subtemp_acs_sf, temp_zc, source_id = "geoid", target_id = 'geoid_zc')
+  predict_civic <- redistribute(subtemp_acs_sf, temp_civic, source_id = "geoid", target_id = 'geoid_ca')
+  predict_hd <- redistribute(subtemp_acs_sf, temp_hd, source_id = "geoid", target_id = 'geoid_hd')
+  
+  # add the year as variable
+  predict_hsr <- predict_hsr %>% mutate(year=vyear) %>% st_drop_geometry()
+  predict_pd <- predict_pd %>% mutate(year=vyear) %>% st_drop_geometry()
+  predict_sd <- predict_sd %>% mutate(year=vyear) %>% st_drop_geometry()
+  predict_zc <- predict_zc %>% mutate(year=vyear) %>% st_drop_geometry()
+  predict_civic <- predict_civic %>% mutate(year=vyear) %>% st_drop_geometry()
+  predict_hd <- predict_hd %>% mutate(year=vyear) %>% st_drop_geometry()
+  
+  # combine aggregation over year
+  predict <- rbind(predict_hsr,
+                   predict_pd,
+                   predict_sd,
+                   predict_zc,
+                   predict_civic,
+                   predict_hd)
+  model <- rbind(model,predict)
+}
+
+# estimate the percentage  --------------------------------------------------
+model_direct <- model %>%
+  mutate(perc_hh_limited_english = 100*(hh_limited_english)/total_hh) %>%
+  pivot_longer(!c('id','year'), names_to = "measure", values_to = "value") %>%
+  filter(!(measure=='total_hh')) %>%
+  mutate(moe='') %>%
+  select(geoid=id,year,measure,value,moe)
+  
+
+# combine the data
+temp_acs_dmg <- acs %>% 
+  select(geoid,year,measure,value,moe) 
+temp_direct_dmg <- model_direct 
+baseline_data <- rbind(temp_acs_dmg,temp_direct_dmg)
+
+# baseline_data <- read_csv('Language/data/working/model/va_hsrsdpdzccttrbg_sdad_2016_2021_language_demographics1.csv.xz')
+# unique(baseline_data$measure)
+# yearlist <- unique(baseline_data$year)
+
+baseline_data <- baseline_data %>% 
+  mutate(measure=case_when(
+    measure=="hh_limited_english" ~ "language_hh_limited_english_count_direct",
+    measure=="perc_hh_limited_english" ~ "language_hh_limited_english_percent_direct")) %>%
+  filter(!is.na(value)) %>%
+  mutate(geoid=as.character(geoid))
+
+
+# save the living units distribution ----------------------------------------------------------------------------
+savepath = "Language/data/working/model/"
+readr::write_csv(baseline_data, xzfile(paste0(savepath,"va_hsrsdpdzccttrbg_sdad_",min(yearlist),'_',max(yearlist),"_language_demographics_direct.csv.xz"), compression = 9))
+
+
+# files = list.files(savepath)
+# filename = files[str_detect(files,"va059_hsrsdpdzc_sdad")]
+# 
+# if (length(filename)==0){
+#   # create the file
+#   readr::write_csv(model_direct, xzfile(paste0(savepath,"va059_hsrsdpdzc_sdad_",min(yearlist),'_',max(yearlist),"_language_demographics.csv.xz"), compression = 9))
+#   
+# }else{
+#   # there is only one file perform check to whether replace the file or update the content
+#   file_maxyear = as.numeric(substr(filename,27,30))
+#   if(max(yearlist)!=file_maxyear){
+#     # update filename
+#     file.remove(filename)
+#     readr::write_csv(model_direct, xzfile(paste0(savepath,"va059_hsrsdpdzc_sdad_",min(yearlist),'_',max(yearlist),"_language_demographics.csv.xz"), compression = 9))
+#     
+#   }else{
+#     #read the file and check if the measure content the model name
+#     lastfile <- read.csv(paste0(savepath,filename))
+#     lastfile <- rbind(lastfile, model_direct)
+#     readr::write_csv(lastfile, xzfile(paste0(savepath,"va059_hsrsdpdzc_sdad_",min(yearlist),'_',max(yearlist),"_language_demographics.csv.xz"), compression = 9))
+#   }
+# }
---
+++ Race/code/distribution/ingest_acs.R
@@ -0,0 +1,249 @@
+# Get the race distribution from ACS for different geographies level
+# Geographic level:   - Tracts
+#                     - Counties
+#                     - Block groups
+
+# Libraries -------------------------------------------------------------------------------------
+library(dplyr)
+library(sf)
+# library(httr)
+library(rjson)
+library(tidyr)
+library(readr)
+library(tidycensus)
+census_api_key(Sys.getenv('census_api_key'))
+
+
+# Upload the data --------------------------------------------------------------------------------
+
+# list of variables
+named_acs_var_list <- c(
+  total_race = "B02001_001",
+  wht_alone = "B02001_002",
+  afr_amer_alone = "B02001_003",
+  native_alone = "B02001_004",
+  asian_alone = "B02001_005",
+  pacific_islander_alone = "B02001_006",
+  other_language = "B02001_007",
+  two_or_more = "B02001_008",
+  pop_eth_tot = "B03003_001", 
+  pop_hispanic_or_latino = "B03003_003")
+
+# list of geography level
+geographies <- c('tract','county','block group')
+
+# list of states
+states <- c('VA','MD','DC')
+
+# list of years
+years <- 2009:2021
+
+# Download the data from ACS for VA and NCR
+# 1. Virginia (all census geographies) . (comments: block groups information are available after 2012)
+acs_data_va_wd <- NULL
+for (geo in geographies){
+  for (year in years){
+    if ((geo=='block group') && (year<2013)){
+      temp <- NULL
+    }else{
+      temp <- data.table::setDT(
+        tidycensus::get_acs(
+          state = "VA",
+          survey = "acs5",
+          year = year,
+          geography = geo,
+          output = "wide",
+          variables = named_acs_var_list
+        )
+      )
+      
+      temp$year <- year
+      temp$region_type <- geo
+      acs_data_va_wd <- rbind(acs_data_va_wd, temp)
+    }
+  }
+}
+
+
+# 2. NCR (all census geographies)
+acs_data_ncr_wd <- NULL
+for (state in states){
+  for (geo in geographies){
+    for (year in years){
+      if ((geo=='block group') && (year<2013)){
+        temp <- NULL
+      }else{
+        temp <- data.table::setDT(
+          tidycensus::get_acs(
+            state = state,
+            survey = "acs5",
+            year = year,
+            geography = geo,
+            output = "wide",
+            variables = named_acs_var_list
+          )
+        )
+        
+        temp$year <- year
+        temp$region_type <- geo
+        acs_data_ncr_wd <- rbind(acs_data_ncr_wd, temp)
+      }
+    }
+  }
+}
+
+
+
+# Compute statistics -----------------------------------------------------------------------------
+
+# 1. Race distribution for VA
+acs_data_va <- acs_data_va_wd %>%
+  mutate(total_race=total_raceE,
+         pop_wht_alone = wht_aloneE, 
+         pop_afr_amer_alone = afr_amer_aloneE, 
+         pop_native_alone = native_aloneE, 
+         pop_AAPI = (asian_aloneE + pacific_islander_aloneE), 
+         pop_other = other_languageE,
+         pop_two_or_more = two_or_moreE,
+         pop_hispanic_or_latino = pop_hispanic_or_latinoE,
+         perc_wht_alone = 100*pop_wht_alone/total_race,
+         perc_afr_amer_alone = 100*pop_afr_amer_alone/total_race,
+         perc_native_alone = 100*pop_native_alone/total_race,
+         perc_AAPI = 100*pop_AAPI/total_race,
+         perc_two_or_more = 100*pop_two_or_more/total_race,
+         perc_other = 100*pop_other/total_race,
+         perc_hispanic_or_latino = 100*pop_hispanic_or_latino/pop_eth_totE) %>%
+  dplyr::select(geoid=GEOID,
+                region_name=NAME,
+                region_type,
+                year,
+                total_race,
+                pop_wht_alone,
+                pop_afr_amer_alone,
+                pop_native_alone,
+                pop_AAPI,
+                pop_other,
+                pop_two_or_more,
+                pop_hispanic_or_latino,
+                perc_wht_alone,
+                perc_afr_amer_alone,
+                perc_native_alone,
+                perc_AAPI,
+                perc_two_or_more,
+                perc_other,
+                perc_hispanic_or_latino) %>%
+  gather(measure, value, -c(geoid, region_name, region_type, year)) %>%
+  select(geoid,region_name,region_type,year,measure,value) %>%
+  mutate(measure_type=case_when(
+    grepl('perc',measure)==T ~ "percentage",
+    grepl('pop',measure)==T ~ "count",
+    grepl('race',measure)==T ~ "count"),
+         moe='') %>%
+  mutate(geoid=format(geoid, scientific = FALSE, justify='none'))
+
+
+#2. Age distribution afor NCR
+acs_data_ncr <- acs_data_ncr_wd %>%
+  mutate(total_race=total_raceE,
+         pop_wht_alone = wht_aloneE, 
+         pop_afr_amer_alone = afr_amer_aloneE, 
+         pop_native_alone = native_aloneE, 
+         pop_AAPI = (asian_aloneE + pacific_islander_aloneE), 
+         pop_other = other_languageE,
+         pop_two_or_more = two_or_moreE,
+         pop_hispanic_or_latino = pop_hispanic_or_latinoE,
+         perc_wht_alone = 100*pop_wht_alone/total_race,
+         perc_afr_amer_alone = 100*pop_afr_amer_alone/total_race,
+         perc_native_alone = 100*pop_native_alone/total_race,
+         perc_AAPI = 100*pop_AAPI/total_race,
+         perc_two_or_more = 100*pop_two_or_more/total_race,
+         perc_other = 100*pop_other/total_race,
+         perc_hispanic_or_latino = 100*pop_hispanic_or_latino/pop_eth_totE) %>%
+  dplyr::select(geoid=GEOID,
+                region_name=NAME,
+                region_type,
+                year,
+                total_race,
+                pop_wht_alone,
+                pop_afr_amer_alone,
+                pop_native_alone,
+                pop_AAPI,
+                pop_other,
+                pop_two_or_more,
+                pop_hispanic_or_latino,
+                perc_wht_alone,
+                perc_afr_amer_alone,
+                perc_native_alone,
+                perc_AAPI,
+                perc_two_or_more,
+                perc_other,
+                perc_hispanic_or_latino) %>%
+  gather(measure, value, -c(geoid, region_name, region_type, year)) %>%
+  select(geoid,region_name,region_type,year,measure,value) %>%
+  mutate(measure_type=case_when(
+    grepl('perc',measure)==T ~ "percentage",
+    grepl('pop',measure)==T ~ "count",
+    grepl('race',measure)==T ~ "count"),
+    moe='',
+    census_year=if_else(year<2020,2010,2020)) %>%
+  mutate(geoid=format(geoid, scientific = FALSE, justify='none'))
+
+
+
+# get the list of tracts, counties and block groups from NCR
+temp_bg2010 <- read_sf('https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/NCR/Census%20Geographies/Block%20Group/2010/data/distribution/ncr_geo_census_cb_2010_census_block_groups.geojson') %>%
+  select(geoid,region_type,year) %>% st_drop_geometry()
+temp_bg2020 <- read_sf('https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/NCR/Census%20Geographies/Block%20Group/2020/data/distribution/ncr_geo_census_cb_2020_census_block_groups.geojson') %>%
+  select(geoid,region_type,year) %>% st_drop_geometry()
+temp_ct2010 <- read_sf('https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/NCR/Census%20Geographies/County/2010/data/distribution/ncr_geo_census_cb_2010_counties.geojson') %>%
+  select(geoid,region_type,year) %>% st_drop_geometry()
+temp_ct2020 <- read_sf('https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/NCR/Census%20Geographies/County/2020/data/distribution/ncr_geo_census_cb_2020_counties.geojson') %>%
+  select(geoid,region_type,year) %>% st_drop_geometry()
+temp_tr2010 <- read_sf('https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/NCR/Census%20Geographies/Tract/2010/data/distribution/ncr_geo_census_cb_2010_census_tracts.geojson') %>%
+  select(geoid,region_type,year) %>% st_drop_geometry()
+temp_tr2020 <- read_sf('https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/NCR/Census%20Geographies/Tract/2020/data/distribution/ncr_geo_census_cb_2020_census_tracts.geojson') %>%
+  select(geoid,region_type,year) %>% st_drop_geometry()
+ncr_geo <- rbind(temp_bg2010,temp_bg2020,temp_ct2010,temp_ct2020,temp_tr2010,temp_tr2020) %>%
+  rename(census_year=year) %>%
+  mutate(geoid=format(geoid, scientific = FALSE, justify='none'))
+
+acs_data_ncr <- merge(acs_data_ncr, ncr_geo, by.x=c('geoid','region_type','census_year'), by.y=c('geoid','region_type','census_year'), all.y=T) %>%
+  select(geoid,region_name,region_type,year,measure,value,measure_type,moe)
+
+acs_data_ncr <- acs_data_ncr %>%
+  mutate(measure=case_when(
+    measure=="total_race" ~ "race_total_count_direct",
+    measure=="pop_wht_alone" ~ "race_wht_alone_count_direct",
+    measure=="pop_afr_amer_alone" ~ "race_afr_amer_alone_count_direct",
+    measure=="pop_native_alone" ~ "race_native_alone_count_direct",
+    measure=="pop_AAPI" ~ "race_AAPI_count_direct",
+    measure=="pop_other" ~ "race_other_count_direct", 
+    measure=="pop_two_or_more" ~ "race_two_or_more_count_direct",
+    measure=="pop_hispanic_or_latino" ~ "race_hispanic_or_latino_count_direct",
+    measure=="perc_wht_alone" ~ "race_wht_alone_percent_direct",
+    measure=="perc_afr_amer_alone" ~ "race_afr_amer_alone_percent_direct",
+    measure=="perc_native_alone" ~ "race_native_alone_percent_direct",
+    measure=="perc_AAPI" ~ "race_AAPI_percent_direct",
+    measure=="perc_two_or_more" ~ "race_two_or_more_percent_direct",
+    measure=="perc_other" ~ "race_other_percent_direct",
+    measure=="perc_hispanic_or_latino" ~ "race_hispanic_or_latino_percent_direct",
+    measure=="pop_eth_tot" ~ "race_eth_tot_count_direct")) %>%
+  filter(!is.na(value)) %>%
+  mutate(geoid=as.character(geoid))
+
+acs_data_ncr_parcels <- acs_data_ncr %>%
+  mutate(measure=str_replace(measure,'direct','parcels'))
+
+acs_data_ncr <- rbind(acs_data_ncr,acs_data_ncr_parcels)
+
+
+# Save the data ----------------------------------------------------------------------------------
+savepath = "Race/data/working/"
+saveRDS(acs_data_va, paste0(savepath,"va_cttrbg_acs_",min(years),'_',max(years),"_race_demographics.csv.xz"), compress = 'xz')
+
+savepath <- "Race/data/distribution/"
+readr::write_csv(acs_data_ncr, xzfile(paste0(savepath,"ncr_cttrbg_acs_",min(years),'_',max(years),"_race_demographics.csv.xz"), compression = 9))
+
+
+
+
